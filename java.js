654d2b4bb9bb24a023ae7a42819cd046bd751b18
Artifiction1 <angelus1@inbox.lv>
2022-05-03 11:24:33 +0300

    string + boolean

--- java.js
+++ java.js
@@ -16,3 +16,15 @@ console.log(integer / 0)
 console.log("string divided by integer")
 console.log("hello" / 15)
 console.log("---------String---------")
+console.log("double quotes")
+console.log('left goinf quotes')
+console.log('single quotes')
+// use \ to work around stirng quotes.
+console.log("in JS, Strings can be delimited using \",\'and\`")
+//make 2 lines \n is a line break
+console.log("---------Boolean---------")
+console.log("this will be \n in 2 lines")
+console.log("this is a boolean:")
+console.log(true)
+let anotherBoolean = false
+console.log(anotherBoolean)
