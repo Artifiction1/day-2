96d9f9b8b68b6d48d843f2993d46bb210683a72c
Artifiction1 <angelus1@inbox.lv>
2022-05-03 10:34:15 +0300

    number examples

---
+++ java.js
@@ -0,0 +1,18 @@
+console.log("---------NUMBERS---------");
+
+let integer = 200
+let floatingPoint = 12.35897
+let negative = -13
+
+console.log("here are an integer, a floating point and a negative:")
+console.log (integer)
+console.log(floatingPoint)
+console.log(negative)
+
+console.log("divide integer with float")
+console.log(integer / floatingPoint)
+console.log("I divide by zero")
+console.log(integer / 0)
+console.log("string divided by integer")
+console.log("hello" / 15)
+console.log("---------String---------")
